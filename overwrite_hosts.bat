5d5e84d30f3f1c2ea23eccc2155e7c0c9f6baab2
Bobby Liu <1181822834@qq.com>
2019-12-01 15:43:19 -0800

    update
    update the hosts address

--- overwrite_hosts.bat
+++ overwrite_hosts.bat
@@ -19,16 +19,16 @@ echo.-------------------------------------------------------------------------
 echo.如有360、电脑管家等安全软件提醒，请勾选信任允许和不再提醒！
 echo.
 echo.警告：执行该命令 您的hosts将被自动替换覆盖！
-echo.如您原先的hosts有自己修改过的重要信息，请自行备份！
+echo.可备份所需内容至cuntom.txt中，将添加到新的hosts的末尾！
 color 2e
 echo.-------------------------------------------------------------------------
 echo.【请输入对应数字序号选择使用】
 echo.
-echo. 1.首次使用/使用普通hosts（Google+FB+Gmail...）
+echo. 1.首次使用（从镜像获取hosts文件）
 echo.
-echo. 2.使用增强hosts（Google+FB+Gmail... + Youtube）
+echo. 2.使用精简版hosts（最低限度访问Google）
 echo.
-echo. 3.使用霸王hosts（Google+FB+Gmail... + Youtube + GooglePlay）
+echo. 3.使用完整版hosts（访问全套Google服务）
 echo.
 echo. 4.恢复默认hosts
 echo.-------------------------------------------------------------------------
@@ -39,9 +39,9 @@ set /p select=请输入数字并按回车键确认:
 
 echo.
 if %select%==4 goto defaultHost
-if %select%==3 goto replaceHostGYP
-if %select%==2 goto replaceHostGY
-if %select%==1 goto replaceHostG
+if %select%==3 goto replaceHostSourceFull
+if %select%==2 goto replaceHostSoureeCompact
+if %select%==1 goto replaceHostFirstTimeFull
 cls
 echo 您输入有误，请重新选择。
 ping 127.0.1 -n "2">nul
@@ -50,34 +50,28 @@ goto main
 :Win7Choice
 choice /c 1234 /n /m "请输入相应数字："
 if errorlevel 4 goto defaultHost
-if errorlevel 3 goto replaceHostGYP
-if errorlevel 2 goto replaceHostGY
-if errorlevel 1 goto replaceHostG
+if errorlevel 3 goto replaceHostSourceFull
+if errorlevel 2 goto replaceHostSoureeCompact
+if errorlevel 1 goto replaceHostFirstTimeFull
 cls
 goto main
 
-:replaceHostG
-wget -q --no-check-certificate https://coding.net/u/scaffrey/p/hosts/git/raw/master/hosts -O "%SystemRoot%\System32\drivers\etc\hosts"
+:replaceHostFirstTimeFull
+wget -q --no-check-certificate https://coding.net/u/scaffrey/p/hosts/git/raw/master/hosts-files/hosts -O "%SystemRoot%\System32\drivers\etc\hosts"
+@echo. >> "%SystemRoot%\System32\drivers\etc\hosts"
+type custom.txt >> "%SystemRoot%\System32\drivers\etc\hosts"
 goto replaceSuccess
 
-:replaceHostGY
-wget -q --no-check-certificate https://raw.githubusercontent.com/racaljk/hosts/master/hosts -O "%SystemRoot%\System32\drivers\etc\hosts"
-wget -q --no-check-certificate https://raw.githubusercontent.com/sy618/hosts/master/y -O hosts_ytb
+:replaceHostSoureeCompact
+wget -q --no-check-certificate https://raw.githubusercontent.com/googlehosts/hosts/master/hosts-files/hosts-compact -O "%SystemRoot%\System32\drivers\etc\hosts"
 @echo. >> "%SystemRoot%\System32\drivers\etc\hosts"
-type hosts_ytb >> "%SystemRoot%\System32\drivers\etc\hosts"
-del hosts_ytb
+type custom.txt >> "%SystemRoot%\System32\drivers\etc\hosts"
 goto replaceSuccess
 
-:replaceHostGYP
-wget -q --no-check-certificate https://raw.githubusercontent.com/racaljk/hosts/master/hosts -O "%SystemRoot%\System32\drivers\etc\hosts"
-wget -q --no-check-certificate https://raw.githubusercontent.com/sy618/hosts/master/y -O hosts_ytb
-wget -q --no-check-certificate https://raw.githubusercontent.com/sy618/hosts/master/p -O hosts_play
+:replaceHostSourceFull
+wget -q --no-check-certificate https://raw.githubusercontent.com/googlehosts/hosts/master/hosts-files/hosts -O "%SystemRoot%\System32\drivers\etc\hosts"
 @echo. >> "%SystemRoot%\System32\drivers\etc\hosts"
-type hosts_ytb >> "%SystemRoot%\System32\drivers\etc\hosts"
-@echo. >> "%SystemRoot%\System32\drivers\etc\hosts"
-type hosts_play >> "%SystemRoot%\System32\drivers\etc\hosts"
-del hosts_ytb
-del hosts_play
+type custom.txt >> "%SystemRoot%\System32\drivers\etc\hosts"
 goto replaceSuccess
 
 :replaceSuccess
--- readme.md
+++ readme.md
@@ -1,13 +1,15 @@
 ## hosts自动替换.bat
 * 适用于Windows的自动更换hosts批处理程序。
-* 首次使用请选择1，使用的是国内coding的源以确保能够访问，完成后会添加Github的hosts，这样后续操作就能使用github的源了。
-* Windows XP双击.bat文件运行。
+* 支持自定义hosts条目，添加到custom.txt即可在替换hosts后添加到末尾。
+* 首次使用请选择1，使用的是国内coding的源以确保能够访问，完成后会添加Github的hosts，这样后续操作就能使用github的源了。使用增强版以上时请保证当前文件夹内没有hosts开头的文件。
+* Windows XP双击.bat文件运行。为避免权限问题，不要在桌面或C盘运行。
 * Win7以上需要右键点击后选择以管理员身份运行。（不过Win10倒是可以直接运行）
 * wget.exe重要！用于下载网上的hosts文件，没有它，.bat就无法正常更新hosts了。
 
 ***
 ### reference
-https://laod.cn/hosts/2017-google-hosts.html
-https://coding.net/u/scaffrey/p/hosts/git
-https://github.com/racaljk/hosts
-https://github.com/sy618/hosts
+* https://github.com/googlehosts/hosts
+* https://laod.cn/hosts/2019-hosts.html
+* https://coding.net/u/scaffrey/p/hosts/git
+* https://github.com/racaljk/hosts
+* https://github.com/sy618/hosts
